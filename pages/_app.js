b159ccf132cee6a436a3dfa011f61998a3b3c0ae
Weslley William Silva Pereira <weswil@hotmail.com>
2025-09-05 20:22:00 -0300

    atualizacao 05092021

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,20 @@
 // NOME DO ARQUIVO: pages/_app.js
-// Ponto de entrada da aplicação, onde o AuthProvider é injetado.
+// Ponto de entrada da aplicação, onde o AuthProvider é injetado e o tema é aplicado.
 
 import { AuthProvider } from '../contexts/AuthContext';
 import '../styles/globals.css';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  // Efeito para aplicar o tema (claro/escuro) no carregamento inicial da aplicação
+  useEffect(() => {
+    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, []);
+
   return (
     <AuthProvider>
       <Component {...pageProps} />
@@ -13,4 +23,3 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp;
-
